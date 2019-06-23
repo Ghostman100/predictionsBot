6e2fb7b63386a91d10b2d0757d75d2a76ef90932
Ghostman100 <gostanin75@gmail.com>
2019-06-23 15:05:40 +0700

    change time

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,7 @@ class ImagesController < ApplicationController
   def index
     o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
     @id = (0...11).map { o[rand(o.length)] }.join
+    @time = params[:time].to_time
   end
 
 end
